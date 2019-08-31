c2adb47f9e2cc258b2f1a82ada62d5a3006618c1
Joey Gaitan <joeygaitan707@gmail.com>
2019-08-30 19:31:06 -0700

    I added a post request that took input information from a form. Then it displayed it to another Html page. I am debating saving it to a JSON file.

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
-from flask import Flask, render_template    
+from flask import Flask, render_template,request
 app = Flask(__name__)
 
 
-playlists = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+stories = [
+    {"first":"Hello ","second":"","third":"","fourth":"","fifth":""},
+    {"first":"Greetings ","second":"","third":"","fourth":"","fifth":""},
+    {"first":"Hola ","second":"","third":"","fourth":"","fifth":""},
+    {"first":"Welcome ","second":"","third":"","fourth":"","fifth":""},
+    {"first":"","second":"","third":"","fourth":"","fifth":""}
 ]
+
+user = {"name":"","gender":""}
+verbs = []
+nouns = []
+adjectives = []
+pronouns=[]
+
 
 @app.route("/")
 def index():
     """Return homepage."""
-    return render_template('home.html', playlists=playlists)
-    
-@app.route("/playlists")
-def playlist():
-    """Return playlists"""
-    return render_template('playlists_index.html', playlists=playlists)
+    return render_template('home.html', stories=stories, verbs=verbs, nouns=nouns, adjectives=adjectives,pronouns=pronouns)
+@app.route('/',methods=['POST'])
+def formHandler():
+    print('hello')
+    first = request.form['first']
+    verbs.append(first)
+    return render_template('base.html',first=first)
+# @app.route("/playlists")
+# def playlist():
+#     """Return playlists"""
+#     return render_template('playlists_index.html', playlists=playlists)
 app.run(debug=True)
 
