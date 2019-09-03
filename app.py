1890f182d87a6217025ec2e97649acfedc847ce5
Joey Gaitan <joeygaitan707@gmail.com>
2019-09-03 15:09:52 -0700

    added in more structure and some comments for the app.py program

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ app = Flask(__name__)
 
 stories = [
     ['Hello ',"Greetings ","Hola "," Nice to finally meet you ", " Welcome "],
-    ["What brings you here ", " We meet again ", " Nice to see you again "," What brings you here "," Its always a pleasure to see you "],
+    [" What brings you here ", " We meet again ", " Nice to see you again "," What brings you here "," Its always a pleasure to see you "],
     [". I bet you don't ",". Would you know the ",". Somehow the ",". The grand scale of the ",". I was amazed by the "],
     [" of these "," of this "],
-    [". Please ",". Somehow can you "," I wonder if you can "," Could you be a dear and "," I will have to "]
+    [". Please finish the ",". Somehow can you complete the "," I wonder if you can destroy this"," Could you be a dear and end the "," I will have to wait and "],
+    ['. If you do this for ', ". If this is done for ", ". If it is achieved by ", ". When it is completed by ", "They will ask when "]
 ]
-
+#This function scrammbles the list of lists
 def storyScrambler(stories):
     newList = []
     for story in stories:
@@ -37,10 +38,9 @@ def formHandler():
     noun = request.form['noun']
     adjective = request.form['adjective']
     verb = request.form['verb']
-    return render_template('base.html',newlist=newlist, pro=pro, noun=noun, adjective=adjective, verb=verb)
-# @app.route("/playlists")
-# def playlist():
-#     """Return playlists"""
-#     return render_template('playlists_index.html', playlists=playlists)
+    noun1 = request.form['noun1']
+    pro1 = request.form['pro1']
+    return render_template('base.html',newlist=newlist, pro=pro, noun=noun, adjective=adjective, verb=verb, noun1 = noun1, pro1=pro1)
+
 app.run(debug=True)
 
